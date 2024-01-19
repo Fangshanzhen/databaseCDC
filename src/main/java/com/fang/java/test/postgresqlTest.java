ab215465f6e54e626770701ef9d03600f8d2410e
fang <pmg0079@dingtalk.com>
2024-01-19 18:07:50 +0800

    20240119

--- src/main/java/com/fang/java/test/postgresqlTest.java
+++ src/main/java/com/fang/java/test/postgresqlTest.java
@@ -1,6 +1,7 @@
 package com.fang.java.test;
 
 import com.fang.java.kafkaToKettle;
+import com.fang.java.kafkaTransform;
 import com.fang.java.kettleResponse;
 import com.fang.java.postgresqlCDCnew;
 
@@ -16,11 +17,11 @@ public class postgresqlTest {
 
 
 
-        postgresqlCDCnew.incrementData("POSTGRESQL", "postgres", "test", "127.0.0.1", "5432", "postgres",
-                "123456","test2","10.0.108.51:9092","test01191");
+//        postgresqlCDCnew.incrementData("POSTGRESQL", "postgres", "test", "127.0.0.1", "5432", "postgres",
+//                "123456","test2","10.0.108.51:9092","test01191");
 
-
-//        kafkaToKettle.createTransMeta("10.0.108.51:9092","test01191");
+//String s="{\"database\":\"postgres\",\"operate_type\":\"update\",\"sqlJson\":{\"column1\":\"12\",\"column4_before\":\"45678\",\"column4\":\"77777\",\"column3\":\"12\",\"column2\":\"12\",\"column2_before\":\"12\",\"column3_before\":\"12\",\"column1_before\":\"12\"},\"operate_ms\":\"2024-01-19 16:20:55\",\"table\":\"test2\"}";
+//        kafkaTransform.createTransMeta(s,"1");
 
     }
 
